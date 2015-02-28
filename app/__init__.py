8e09268dffa7cb0d37855b66bd1b28b97986daa5
samorae101@gmail.com <samorae101@gmail.com>
2015-02-28 03:25:03 +0000

    added db info

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://action@localhost/action'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://kwuoxfieyolizi:KkHVx445S2fFDxZ9KZnW87nfKh@ec2-23-23-180-133.compute-1.amazonaws.com:5432/dabrcfl2c5jikk'
 
 db = SQLAlchemy(app)
 
